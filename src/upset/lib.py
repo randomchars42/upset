912cbfae798bc734e5ab3434b3cc762f286c2565
randomchars42 <randomchars42@users.noreply.github.com>
2022-10-17 12:13:01 +0200

    provide functions to interact with the filesystem

--- src/upset/lib.py
+++ src/upset/lib.py
@@ -1,176 +1,311 @@
 """Library providing basic functionality and a base class for plugins."""
 
 import configparser
+import dataclasses
+import grp
 import logging
+import os
 import pathlib
+import pwd
+import re
+import stat
+import string
 
 logger: logging.Logger = logging.getLogger()
+
+@dataclasses.dataclass
+class Template():
+    """Holds a template string and its variables.
+
+    Attributes:
+        string: The file with `$variables` to use as content.
+        substitutes: A dictionary with the variable names as keys and their
+            substitutes as values.
+    """
+    file: pathlib.Path
+    substitutes: dict[str, str]
+
+@dataclasses.dataclass
+class PermissionSet():
+    """Holds a basic representation of UNIX permissions.
+
+    Attributes:
+        mod: The file mode (octal value; default `0o600`).
+        owner: The file owner (existence will be checked; `''` to leave as is).
+        group: The group (existence will not be checked; `''` to leave as is).
+    """
+    mode: int = 0o600
+    owner: str = ''
+    group: str = ''
+
+class UpsetFsError(Exception):
+    """Error for Fs interactions."""
 
 class Fs:
     """Filesystem related functions."""
 
     @staticmethod
-    def backup(path: pathlib.Path) -> None:
-        """Backup a file by moving it to "FILENAME(.SUFFIX).bk".
+    def backup(path: pathlib.Path, number: int = 0) -> None:
+        """Backup a file by moving it to "FILENAME~" or "FILENAME~NUM~".
 
         Args:
             path: The path to the file to backup.
-        """
-        try:
-            if not path.is_file():
-                logger.error('cannot backup file "%s"', str(path))
-            path.replace(pathlib.Path(str(path) + '.bk'))
-        except OSError as error:
-            logger.error('could not backup "%s" (%s)', str(path), str(error))
+            number: The number of the backup. Do not change this.
 
+        Raises:
+            UpsetFsError: If filesystem interaction fails.
+        """
+        if not (path.is_file() or path.is_dir()):
+            return
+
+        suffix: str = f'~{number}~' if number > 0 else '~'
+        backup_path: pathlib.Path = pathlib.Path(str(path) + suffix)
+
+        if backup_path.exists():
+            Fs.backup(path, number + 1)
+            return
+
+        logger.info('creating backup for %s', str(path))
+        try:
+            path.rename(backup_path)
+        except OSError as error:
+            raise UpsetFsError(f'could not backup "{path}"') from error
 
     @staticmethod
-    def ensure_file(path: pathlib.Path, mode: int, owner: str, group: str,
-            template: str = '', substitutes: dict[str, str] = {},
-            force_template: bool = False, backup: bool = True):
-        """
+    def remove(path: pathlib.Path, backup: bool = True) -> None:
+        """Remove the file or move it to "FILENAME(.SUFFIX).bk".
 
         Args:
             path: The path to ensure a file.
-            mode: The file mode (octal value).
-            owner: The file owner (existence will not be checked / ensured).
-            group: The group (existence will not be checked / ensured).
-            template: A template to be used to fill the file.
-            substitutes: Substitute occurences of the given variables by their
-                values.
-            force_templates: Apply template even if the file exists (overwrites
-                the file).
             backup: Create a backup (see `Fs.backup()`).
+
+        Raises:
+            UpsetFsError: If filesystem interaction fails.
         """
+        if backup:
+            Fs.backup(path)
 
-        template = Template(template).safe_substitute(substitutes)
+        # the file might have been moved so there is nothing left to be done
+        if not path.exists():
+            return
 
-        if path.exists():
-            if not path.is_file() or (force_template and template != path.read_text()):
-                self._remove(path, True)
-                create = True
+        logger.info('removing path %s', str(path))
+        try:
+            if path.is_dir():
+                path.rmdir()
             else:
-                create = False
-                logging.debug(' - File {0} present'.format(str(path)))
-        else:
-            if path.is_symlink():
-                self._remove(path, False)
-            create = True
-        if create:
-            logger.info(' - Creating file: {0}'.format(str(path)))
-            path.write_text(template)
-
-        self._ensure_perms(path, mode, owner, group)
-
-    def _ensure_link(self, path, target, backup, mode, owner, group):
-        if path.is_symlink():
-            if path.exists() and path.resolve().samefile(target):
-                logging.debug(' - Symlink {0} present'.format(str(path)))
-                create = False
-            else:
-                self._remove(path, False)
-                create = True
-        elif path.exists():
-            self._remove(path, True)
-            create = True
-        else:
-            create = True
-
-        if create:
-            logger.info(' - Creating symlink: {0}'.format(str(path)))
-            path.symlink_to(target)
-
-        self._ensure_perms(path, mode, owner, group)
-
-    def _ensure_dir(self, path, backup, mode, owner, group):
-        if path.exists():
-            if not path.is_dir():
-                self._remove(path, True)
-                create = True
-            else:
-                create = False
-                logging.debug(' - Directory {0} present'.format(str(path)))
-        else:
-            if path.is_symlink():
-                self._remove(path, False)
-            create = True
-
-        if create:
-            logger.info(' - Creating directory: {0}'.format(str(path)))
-            path.mkdir()
-
-        self._ensure_perms(path, mode, owner, group)
-
-    def _ensure_perms(self, path, mode, owner, group):
-        current_mode = oct(S_IMODE(path.lstat().st_mode))
-
-        if not path.is_symlink():
-            logging.debug(
-                ' - File is: {0}; should be {1}'.format(current_mode, oct(mode)))
-            if current_mode != oct(mode):
-                logger.info(' - Updating permissions of file {0}'.format(str(path)))
-                os.chmod(str(path), mode)#, follow_symlinks=False)
-            if path.owner() != owner:
-                owner = pwd.getpwnam(owner).pw_uid
-                logger.info(' - Updating owner of file {0}'.format(str(path)))
-                os.chown(str(path), uid=owner, gid=-1, follow_symlinks=False)
-            if path.group() != group:
-                group = grp.getgrnam(group).gr_gid
-                logger.info(' - Updating group of file {0}'.format(str(path)))
-                os.chown(str(path), uid=-1, gid=group, follow_symlinks=False)
-
-    def _remove(self, path, backup):
-        if path.exists() or path.is_symlink():
-            if backup:
-                path.rename(str(path) + '.bk')
-            else:
-                logger.info(' - Removing file {0}'.format(str(path)))
                 path.unlink()
+        except OSError as error:
+            raise UpsetFsError(f'could not delete "{path}"') from error
 
-    def _is_user_path(self, path):
-        return True if not re.search('\$USER', str(path)) is None else False
+    @staticmethod
+    def ensure_file(path: pathlib.Path, template: Template,
+            permissions: PermissionSet,
+            mode: str = 'force', backup: bool = True) -> None:
+        """Make sure a file exists.
 
-    def ensure(self, path='', type='', target='', template='', state='present',
-        backup=True, mode=0o600, owner='$USER', group='$USER', users=[],
-        force_template=False, substitutes={}):
+        If the file does not exist it will be created by interpolating the
+        template string. If the file exists and `force_template` is `True`
+        (default) it will be replaced using the template string. If anything but
+        a symlink exists it will be backed up (see `Fs.backup()`) depending on
+        `backup`.
 
-        path = Path(path)
-        target = Path(target)
+        Args:
+            path: The path to ensure a file.
+            template: A template to be used to fill the file with.
+            permissions: The permissions of the file.
+            mode: What to do if the file exists ['asis'|'force'|'update']?
+                'as_is': will leave the file as is if it exists.
+                'force': will re-write the file.
+                'update': If the source is newer the target will be replaced
+                    (default).
+            backup: Create a backup (see `Fs.backup()`; default is `True`).
 
-        is_user_path = self._is_user_path(path)
-        if is_user_path and len(users) == 0:
-            raise ValueError('No users given, but variable $USER used.')
-        elif len(users) == 0:
-            users.append(owner)
+        Raises:
+            UpsetFsError: If filesystem interaction fails.
+        """
+        logger.info('ensuring file "%s"', str(path))
+        if path.is_dir():
+            logger.warning('creating file instead of directory %s',
+                    str(path))
+        elif path.is_file():
+            if mode == 'asis':
+                logger.debug('target "%s" exists and mode "asis"', str(path))
+                return
+            if mode != 'force':
+                try:
+                    source_mtime: float = template.file.stat().st_mtime
+                    target_mtime: float = path.stat().st_mtime
+                except OSError as error:
+                    raise UpsetFsError(
+                            'could not get mtime') from error
+                if target_mtime > source_mtime:
+                    logger.debug('target "%s" newer than source', str(path))
+                    return
 
-        for user in users:
-            pth = Path(re.sub('\$USER', user, str(path)))
-            trgt = Path(re.sub('\$USER', user, str(target)))
-            ownr = re.sub('\$USER', user, owner)
-            grp = re.sub('\$USER', user, group)
-            subs = {}
-            subs = substitutes
-            subs['USER'] = user
+        # remove or backup the file if it exists
+        Fs.remove(path, backup)
 
-            logger.info(
-                'Fs: Ensure {0} at {1} is {2}'.format(type, pth, state))
+        try:
+            content: str = string.Template(
+                template.file.read_text(encoding='utf-8')).safe_substitute(
+                        template.substitutes)
+        except OSError as error:
+            raise UpsetFsError(
+                    'could not open template "{template.file}"') from error
 
-            if state == 'present':
-                if type == 'file':
-                    self._ensure_file(
-                        pth, template, backup, mode, ownr, grp, force_template, subs)
-                elif type == 'dir':
-                    self._ensure_dir(pth, backup, mode, ownr, grp)
-                elif type == 'symlink':
-                    self._ensure_link(pth, trgt, backup, mode, ownr, grp)
+        logger.info('creating file: %s', str(path))
+        try:
+            path.write_text(content, encoding='utf-8')
+        except OSError as error:
+            raise UpsetFsError(f'could not create file "{path}"') from error
+        Fs.ensure_perms(path, permissions)
+
+    @staticmethod
+    def ensure_link(path: pathlib.Path, target: pathlib.Path,
+            backup: bool = True) -> None:
+        """Make sure symlink exists.
+
+        If the symlink does not exist it will be created. If anything but
+        a symlink exists it will be backed up (see `Fs.backup()`) depending on
+        `backup`.
+
+        Args:
+            path: The path to ensure a symlink exists.
+            target: The target of the symlink.
+            backup: Create a backup (see `Fs.backup()`; default is `True`).
+
+        Raises:
+            UpsetFsError: If filesystem interaction fails.
+        """
+        logger.info('ensuring symlink "%s" tp "%s"', str(path), str(target))
+        if path.is_symlink():
+            if path.resolve().samefile(target):
+                logging.debug('symlink "%s" already present', str(path))
+                return
+            path.unlink()
+        elif path.exists():
+            Fs.remove(path, backup)
+
+        logger.info('creating symlink "%s"', str(path))
+        path.symlink_to(target)
+
+    @staticmethod
+    def ensure_dir(path: pathlib.Path, permissions: PermissionSet,
+            backup: bool) -> None:
+        """Make sure directory exists.
+
+        If the directory does not exist it will be created. If anything but
+        a symlink exists it will be backed up (see `Fs.backup()`) depending on
+        `backup`.
+
+        Args:
+            path: The path to ensure a symlink exists.
+            permissions: The permissions of the file.
+            backup: Create a backup (see `Fs.backup()`; default is `True`).
+
+        Raises:
+            UpsetFsError: If filesystem interaction fails.
+        """
+        logger.info('ensuring directory "%s"', str(path))
+        if path.is_dir():
+            logging.debug('directory "%s" already present', str(path))
+            Fs.ensure_perms(path, permissions)
+            return
+
+        Fs.remove(path, backup)
+
+        logger.info('creating directory "%s"', str(path))
+        try:
+            path.mkdir()
+        except OSError as error:
+            raise UpsetFsError(
+                    f'could not create directory "{path}"') from error
+
+        Fs.ensure_perms(path, permissions)
+
+    @staticmethod
+    def ensure_perms(path: pathlib.Path, permissions: PermissionSet) -> None:
+        """Ensure a file or directory has the given permissions.
+
+        Args:
+            path: The path to ensure a file.
+            permissions: The permissions to apply
+
+        Raises:
+            UpsetFsError: If filesystem interaction fails.
+        """
+        current_mode = oct(stat.S_IMODE(path.lstat().st_mode))
+
+        if path.is_symlink():
+            logger.warning('cannot change permissions on symlink "%s"',
+                    str(path))
+            return
+
+        logging.debug('current mode: %s; should be %s', str(current_mode),
+                str(permissions.mode))
+
+        try:
+            if current_mode != oct(permissions.mode):
+                logger.info('changing permissions of file "%s"', str(path))
+                path.chmod(permissions.mode)
+            if path.owner() != permissions.owner:
+                if permissions.owner == '':
+                    owner: int = -1
                 else:
-                    raise ValueError('Invalid type')
-            elif state == 'absent':
-                self._remove(pth, backup)
-            else:
-                raise ValueError('Invalid state')
+                    owner = pwd.getpwnam(permissions.owner).pw_uid
+                logger.info('changing owner of file "%s"', str(path))
+                os.chown(str(path), uid=owner, gid=-1, follow_symlinks=False)
+            if path.group() != permissions.group:
+                if permissions.group == '':
+                    group: int = -1
+                else:
+                    group = grp.getgrnam(permissions.group).gr_gid
+                logger.info('changing group of file "%s"', str(path))
+                os.chown(str(path), uid=-1, gid=group, follow_symlinks=False)
+        except OSError as error:
+            raise UpsetFsError(
+                    f'could not change permissions on "{path}"') from error
 
-        return
+    @staticmethod
+    def ensure_in_file(path: pathlib.Path, text: str, insert_at: str = r'\Z',
+            backup = True) -> None:
+        """Ensure a string occcurs in a file.
+
+        Args:
+            path: The path to ensure a file.
+            text: The text that must occur in the file.
+            insert_at: The position where the text must occur. Must be a valid
+                regular expression. Default is to much the end of the file
+                appending the text to the file.
+            backup: Create a backup (see `Fs.backup()`; default is `True`).
+
+        Raises:
+            UpsetFsError: If filesystem interaction fails.
+        """
+        try:
+            haystack: str = path.read_text(encoding='utf-8')
+        except OSError as error:
+            raise UpsetFsError(
+                    f'could not read file "{path}"') from error
+
+        logger.info('ensuring "%s" is in "%s"', text.split('\n')[0], str(path))
+
+        if re.search(text, haystack):
+            logger.debug('text is already in the file')
+            return
+
+        if backup:
+            Fs.backup(path)
+
+        logger.info('inserting "%s" into "%s"', text.split('\n')[0], str(path))
+        haystack = re.sub(insert_at, text, haystack)
+
+        try:
+            path.write_text(haystack, encoding='utf-8')
+        except OSError as error:
+            raise UpsetFsError(
+                    f'could not write file "{path}"') from error
 
 
 class Plugin:
