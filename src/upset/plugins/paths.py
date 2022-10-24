50a37199a9fd25ff03ffaba91a6edcb44c8baafc
randomchars42 <randomchars42@users.noreply.github.com>
2022-10-24 20:30:06 +0200

    implement / test paths plugin

--- src/upset/plugins/paths.py
+++ src/upset/plugins/paths.py
@@ -62,6 +62,12 @@ Examples::
                     "ensure": "symlink",
                     "backup": true,
                 },
+                # describe an absent file
+                {
+                    "path": "/home/$user/dir",
+                    "ensure": "absent",
+                    "backup": true,
+                },
             ],
             "template2": {
                 "greeting": "Hello",
@@ -82,6 +88,128 @@ Examples::
     }
 """
 
+import logging
 import pathlib
+import sys
 
+from typing import Any
 from upset import lib
+
+# create console handler and set level to debug
+logging_handler: logging.StreamHandler = logging.StreamHandler()
+logging_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(levelname)s %(name)s %(message)s')
+logging_handler.setFormatter(formatter)
+root_logger: logging.Logger = logging.getLogger()
+root_logger.setLevel(logging.DEBUG)
+root_logger.addHandler(logging_handler)
+logger: logging.Logger = logging.getLogger(__name__)
+
+class Paths(lib.Plugin):
+    """Deal with path related tasks."""
+
+    def run(self) -> None:
+        """Do the main work."""
+        for subtask in self.data['variables']['paths']:
+            if not 'backup' in subtask:
+                subtask['backup'] = True
+
+            match subtask['ensure']:
+                case 'absent':
+                    self.ensure_absent(subtask)
+                case 'file':
+                    self.ensure_file(subtask)
+                case 'dir':
+                    self.ensure_dir(subtask)
+                case 'symlink':
+                    self.ensure_symlink(subtask)
+                case 'other':
+                    raise lib.UpsetError(
+                            f'no such subtask "{subtask["ensure"]}"')
+
+    def ensure_absent(self, subtask: Any) -> None:
+        """Ensure nothing exists at path.
+
+        Args:
+            subtask: The object in the `Task.variables.paths` list.
+        """
+        lib.Fs.remove(pathlib.Path(subtask['path']), subtask['backup'])
+
+    def ensure_file(self, subtask: Any) -> None:
+        """Ensure a file is present.
+
+        Args:
+            subtask: The object in the `Task.variables.paths` list.
+        """
+        if 'permissions' in subtask:
+            permissions: lib.PermissionSet = lib.PermissionSet(
+                    *subtask['permissions'])
+        else:
+            permissions = lib.PermissionSet('', '', 0o600)
+
+        if (not 'template' in subtask or
+                not subtask['template'] in self.data['files']):
+            raise lib.UpsetError(f'no template file for "{subtask["file"]}"'
+                    f' @ "{self.data["name"]}"')
+
+        file: pathlib.Path = pathlib.Path(
+                self.data['files'][subtask['template']])
+
+        if subtask['template'] in self.data['variables']:
+            variables: dict[str, str] = \
+                    self.data['variables'][subtask['template']]
+        else:
+            variables = {}
+
+        template: lib.Template = lib.Template(file, variables)
+
+        if 'mode' in subtask:
+            mode:str = subtask['mode']
+        else:
+            mode = 'update'
+
+        lib.Fs.ensure_file(
+                pathlib.Path(subtask['path']),
+                template,
+                permissions,
+                mode,
+                subtask['backup'])
+
+    def ensure_dir(self, subtask: Any) -> None:
+        """Ensure a file is present.
+
+        Args:
+            subtask: The object in the `Task.variables.paths` list.
+        """
+        if 'permissions' in subtask:
+            permissions: lib.PermissionSet = lib.PermissionSet(
+                    *subtask['permissions'])
+        else:
+            permissions = lib.PermissionSet('', '', 0o700)
+
+        lib.Fs.ensure_dir(
+                pathlib.Path(subtask['path']),
+                permissions,
+                subtask['backup'])
+
+    def ensure_symlink(self, subtask: Any) -> None:
+        """Ensure a file is present.
+
+        Args:
+            subtask: The object in the `Task.variables.paths` list.
+        """
+        lib.Fs.ensure_link(
+                pathlib.Path(subtask['path']),
+                pathlib.Path(subtask['target']),
+                subtask['backup'])
+
+if __name__ == '__main__':
+    paths: Paths = Paths()
+    try:
+        paths.run()
+    except lib.UpsetError as error:
+        logger.error(error)
+        sys.exit(1)
+    except KeyError as error:
+        logger.error(error)
+        sys.exit(1)
---
+++ tests/test_plugins_paths_unittest.py
@@ -0,0 +1,164 @@
+"""Test plugin "Paths"."""
+
+import logging
+import logging.config
+import pathlib
+import sys
+import unittest
+
+from typing import Any
+from unittest import mock
+from upset import lib
+from upset.plugins import paths
+
+logging_handler: logging.StreamHandler = logging.StreamHandler()
+logging_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(levelname)s %(name)s %(message)s')
+logging_handler.setFormatter(formatter)
+root_logger: logging.Logger = logging.getLogger()
+root_logger.setLevel(logging.ERROR)
+root_logger.addHandler(logging_handler)
+logger: logging.Logger = logging.getLogger(__name__)
+
+# pylint: disable=too-many-public-methods
+class TestPluginsPaths(unittest.TestCase):
+    """Test Paths from plugins."""
+
+    def setUp(self) -> None:
+        """Add some files."""
+        self._base_dir: pathlib.Path = pathlib.Path('tests/tmp')
+        try:
+            self._base_dir.mkdir()
+        except OSError:
+            logger.debug('could not create "%s"', self._base_dir)
+
+        self._task: Any = {
+            'name': 'ensure my paths',
+            'plugin': 'paths',
+            'variables': {
+                'paths': [
+                    {
+                        'path': '/home/$user/file',
+                        'ensure': 'file',
+                        'template': 'template2',
+                        'mode': 'update',
+                        'permissions': ['$user', '', 0o600],
+                        'backup': True,
+                    },
+                    {
+                        'path': '/home/$user/dir',
+                        'ensure': 'dir',
+                        'permissions': ['$user', '', 0o600],
+                        'backup': True,
+                    },
+                    {
+                        'path': '/home/$user/dir',
+                        'target': '/home/$user/.hidden/dir',
+                        'ensure': 'symlink',
+                        'backup': True,
+                    },
+                ],
+                'template1': {
+                    'greeting': 'Greetings',
+                    'caller': 'Admin',
+                },
+            },
+            'foreach': ['user1', 'user2'],
+            'foreach_variable': 'user',
+            'files': {
+                'template1': str(self._base_dir / 't'),
+                'template2': '/template/with/variables',
+            }
+            }
+
+        # create a mock argument containing the task so that
+        # `lib.Plugins.get_data()` finds and decodes it
+        with mock.patch.object(
+                sys, 'argv',
+                ['paths.py', lib.Helper.encode_data(self._task)]):
+            self._paths = paths.Paths()
+
+    def tearDown(self) -> None:
+        """Remove files."""
+        for file in self._base_dir.glob('*'):
+            try:
+                if file.is_dir():
+                    file.rmdir()
+                else:
+                    file.unlink()
+            except OSError:
+                logger.debug('could not remove "%s"', file)
+        try:
+            self._base_dir.rmdir()
+        except OSError:
+            logger.debug('could not remove "%s"', self._base_dir)
+
+    def test_ensure_absent(self) -> None:
+        """Ensure file is absent by creating a backup."""
+        pathlib.Path(self._base_dir / 'a').touch()
+        #self._paths.ensure_absent(self._task['variables']['paths'][4])
+        self._paths.ensure_absent({
+            'path': str(self._base_dir / 'a'),
+            'ensure': 'absent',
+            'backup': True
+            })
+        self.assertTrue(pathlib.Path(self._base_dir / 'a~').exists())
+        self.assertFalse(pathlib.Path(self._base_dir / 'a').exists())
+
+    def test_ensure_file(self) -> None:
+        """Ensure file exists."""
+        pathlib.Path(self._base_dir / 't').write_text('$greeting, oh $caller!',
+                encoding='utf-8')
+        pathlib.Path(self._base_dir / 'a').touch()
+        self._paths.ensure_file({
+            'path': str(self._base_dir / 'a'),
+            'ensure': 'file',
+            'template': 'template1',
+            'mode': 'force',
+            'permissions': ['', '', 0o644],
+            'backup': True
+            })
+        self.assertTrue(pathlib.Path(self._base_dir / 'a').exists())
+        self.assertTrue(pathlib.Path(self._base_dir / 'a~').exists())
+        self.assertEqual(
+                pathlib.Path(self._base_dir / 'a').read_text(encoding='utf-8'),
+                'Greetings, oh Admin!')
+    def test_ensure_file_no_template(self) -> None:
+        """Fail because no valid template exists."""
+        with self.assertRaises(KeyError):
+            self._paths.ensure_file({
+                'path': str(self._base_dir / 'a'),
+                'ensure': 'file',
+                'template': 'template',
+                'mode': 'force',
+                'permissions': ['', '', 0o644],
+                'backup': True
+                })
+
+    def test_ensure_link(self) -> None:
+        """Ensure link exists."""
+        pathlib.Path(self._base_dir / 't').touch()
+        pathlib.Path(self._base_dir / 'a').touch()
+        self._paths.ensure_symlink({
+            'path': str(self._base_dir / 'a'),
+            'ensure': 'symlink',
+            'target': str(self._base_dir / 't'),
+            'backup': True
+            })
+        self.assertTrue(pathlib.Path(self._base_dir / 'a').is_symlink())
+        self.assertTrue(pathlib.Path(self._base_dir / 'a~').exists())
+
+    def test_ensure_dir(self) -> None:
+        """Ensure link exists."""
+        pathlib.Path(self._base_dir / 'a').touch()
+        self._paths.ensure_dir({
+            'path': str(self._base_dir / 'a'),
+            'ensure': 'dir',
+            'permissions': ['', '', 0o644],
+            'backup': True
+            })
+        self.assertTrue(pathlib.Path(self._base_dir / 'a').is_dir())
+        self.assertTrue(pathlib.Path(self._base_dir / 'a~').exists())
+
+if __name__ == '__main__':
+    unittest.main()
